f5d072c33ff5522229f3a8c947d2bfecf370ed97
annpatrica <agerbeth21@gmail.com>
2022-06-22 18:26:30 -0400

    Added get /items

--- Section_4_Flask_RESTful/app.py
+++ Section_4_Flask_RESTful/app.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        data = request.get_json()  # can use silent or force as inputs
+        price = data['price']
+        print('Adding price: ', price, 'for item: ', name)
+        item = {'name': name, 'price': price}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000)
