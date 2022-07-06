70ec61825de01ebdba5857d805b50f209ed34211
annpatrica <agerbeth21@gmail.com>
2022-07-06 09:33:29 -0400

    Authorization and error handling implemented

--- Section_4_Flask_RESTful/app.py
+++ Section_4_Flask_RESTful/app.py
@@ -1,20 +1,34 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'jan'
 api = Api(app)
+
+# JWT json web token
+# creates /auth endpoint, this will send username/password and send to authenticate() function
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
-
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        # filter() returns a filter object, can call a number of functions on the response
+        # item = list(filter(lambda x : x['name'] ==  name, items))
+        # next will return the first item found by the filter function, in ours there will only be one item found
+        # adding None is the defualt return
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        # check if we already added this name to our list, return 400 to indicate user error
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with nane '{}' already exists".format(name)}, 400
+
         data = request.get_json()  # can use silent or force as inputs
         price = data['price']
         print('Adding price: ', price, 'for item: ', name)
---
+++ Section_4_Flask_RESTful/user.py
@@ -0,0 +1,7 @@
+class User: 
+    def __init__(self, _id, _username, _passowrd):
+        self.id = _id
+        self.username = _username
+        self.password = _passowrd
+
+    
